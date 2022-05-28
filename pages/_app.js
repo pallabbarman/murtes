5a0d45be569b28dfe58070128b2aea0e90760f3f
Pallab Barman <76774117+pallabbarman@users.noreply.github.com>
2022-05-28 22:40:04 +0600

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import "bootstrap/dist/css/bootstrap.css";
+import { useEffect } from "react";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    useEffect(() => {
+        import("bootstrap/dist/js/bootstrap");
+    }, []);
+
+    return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
