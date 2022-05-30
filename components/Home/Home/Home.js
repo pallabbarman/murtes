7147743a215b928370e9cb9f7c58125060614d66
Pallab Barman <76774117+pallabbarman@users.noreply.github.com>
2022-05-30 20:36:47 +0600

    template added

--- components/Home/Home/Home.js
+++ components/Home/Home/Home.js
@@ -2,12 +2,14 @@ import React from "react";
 import Banner from "../Banner/Banner";
 import Blog from "../Blog/Blog";
 import Experience from "../Experience/Experience";
+import Template from "../Template/Template";
 
 const Home = () => {
     return (
         <>
             <Banner />
             <Experience />
+            <Template />
             <Blog />
         </>
     );
---
+++ components/Home/Template/Template.js
@@ -0,0 +1,62 @@
+import React from "react";
+import classes from "./Template.module.css";
+import Image from "next/image";
+import { BsArrowRight } from "react-icons/bs";
+
+const Template = () => {
+    return (
+        <section className={classes.template}>
+            <div className="container">
+                <div className="row">
+                    <div className={`col-lg-6 ${classes.templateImg}`}>
+                        <Image
+                            src="/images/template/template.png"
+                            alt="person"
+                            width={360}
+                            height={400}
+                            layout="responsive"
+                        />
+                    </div>
+                    <div className={`col-lg-6 mt-5 ${classes.templateItems}`}>
+                        <h1>
+                            Murtes <span> is the first and </span> powerful
+                            template co.
+                        </h1>
+                        <p>
+                            But I must explain to you how all this mistaken
+                            denouncing praising pain was born and I will give
+                            you
+                        </p>
+                        <h1>+00 856 434 862</h1>
+                        <button className="common-btn">
+                            Contact Now <BsArrowRight />
+                        </button>
+                        <div className={` row ${classes.templateFooter}`}>
+                            <div className="col-md-4">
+                                <Image
+                                    src="/images/template/tmp.jpg"
+                                    alt="person"
+                                    width={250}
+                                    height={150}
+                                    layout="responsive"
+                                />
+                            </div>
+                            <div
+                                className={` col-md-8 ${classes.templateFooterItems2}`}
+                            >
+                                <p>
+                                    All the Lorem Ipsum generators on the Intern
+                                    tend to repeat predefined chunks as necessa
+                                    making this the first true generator on the
+                                    model sentence structures.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default Template;
