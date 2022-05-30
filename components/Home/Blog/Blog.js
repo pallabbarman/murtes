e6f3e7cb289f4f9a369a9da130f2710125fd0b69
Pallab Barman <76774117+pallabbarman@users.noreply.github.com>
2022-05-30 19:42:03 +0600

    blog added

---
+++ components/Home/Blog/Blog.js
@@ -0,0 +1,77 @@
+import Image from "next/image";
+import React from "react";
+import classes from "./Blog.module.css";
+import { IoMdPerson } from "react-icons/io";
+import { GoCalendar } from "react-icons/go";
+
+const Blog = () => {
+    const blogData = [
+        {
+            title: "This is Standard blog design for Digital",
+            des: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestias ullam id amet error esse reiciendis.",
+            img: "/images/blog/blog1.jpg",
+            by: "Pallab",
+            date: new Date().toLocaleDateString(),
+        },
+        {
+            title: "It uses a dictionary over words combined",
+            des: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestias ullam id amet error esse reiciendis.",
+            img: "/images/blog/blog2.jpg",
+            by: "Pallab",
+            date: new Date().toLocaleDateString(),
+        },
+        {
+            title: "Old cameras can capture images better!",
+            des: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestias ullam id amet error esse reiciendis.",
+            img: "/images/blog/blog3.jpg",
+            by: "Pallab",
+            date: new Date().toLocaleDateString(),
+        },
+    ];
+
+    return (
+        <section className={classes.blog}>
+            <div className="container">
+                <div className={`${classes.blogHeader}`}>
+                    <h1>
+                        Our <span>blog list and</span> latest news
+                    </h1>
+                    <p>
+                        But I must explain to you how all this mistaken
+                        denouncing praising pain was born and I will give you
+                    </p>
+                </div>
+                <div className={`${classes.blogBody}`}>
+                    {blogData.map((data) => (
+                        <div
+                            className={`card ${classes.blogCard}`}
+                            key={data.title}
+                        >
+                            <div className={`${classes.cardHeader}`}>
+                                <h3>{data.title}</h3>
+                                <p>{data.des}</p>
+                            </div>
+                            <Image
+                                src={data.img}
+                                alt={data.title}
+                                width={300}
+                                height={200}
+                                layout="responsive"
+                            />
+                            <div className={`${classes.cardFooter}`}>
+                                <p>
+                                    <IoMdPerson /> {data.by}
+                                </p>
+                                <p>
+                                    <GoCalendar /> {data.date}
+                                </p>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default Blog;
--- components/Home/Home/Home.js
+++ components/Home/Home/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Banner from "../Banner/Banner";
+import Blog from "../Blog/Blog";
 import Experience from "../Experience/Experience";
 
 const Home = () => {
@@ -7,6 +8,7 @@ const Home = () => {
         <>
             <Banner />
             <Experience />
+            <Blog />
         </>
     );
 };
